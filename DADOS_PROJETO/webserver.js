469a83e835b51445421a644f62b45233696574d8
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-10-12 20:49:08 +0000

    Ataulização do código da aplicação web

--- DADOS_PROJETO/webserver.js
+++ DADOS_PROJETO/webserver.js
@@ -13,17 +13,17 @@ const pool = new Pool({
   port: 5432 // Porta padrão do PostgreSQL
 });
 
-// Rota para adicionar um novo aluno
+// Rota para criar um novo aluno
 app.post('/alunos', (req, res) => {
   const { nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala } = req.body;
-  const sql = 'INSERT INTO alunos (ID, nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala) VALUES ($1, $2, $3, $4, $5, $6)';
-  const values = [ID, nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala];
-  pool.query(sql, values, (err) => {
+  const sql = 'INSERT INTO alunos (nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+  const values = [nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala];
+  pool.query(sql, values, (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
     }
-    res.json({ message: 'Aluno adicionado com sucesso' });
+    res.status(201).json(result.rows[0]);
   });
 });
 
@@ -35,7 +35,60 @@ app.get('/alunos', (req, res) => {
       res.status(500).json({ error: err.message });
       return;
     }
-    res.json({ alunos: result.rows });
+    res.json(result.rows);
+  });
+});
+
+// Rota para obter um aluno por ID
+app.get('/alunos/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = 'SELECT * FROM alunos WHERE id = $1';
+  pool.query(sql, [id], (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: 'Aluno não encontrado' });
+      return;
+    }
+    res.json(result.rows[0]);
+  });
+});
+
+// Rota para atualizar um aluno por ID
+app.put('/alunos/:id', (req, res) => {
+  const id = req.params.id;
+  const { nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala } = req.body;
+  const sql = 'UPDATE alunos SET nome = $1, idade = $2, nota_primeiro_semestre = $3, nota_segundo_semestre = $4, nome_professor = $5, numero_sala = $6 WHERE id = $7 RETURNING *';
+  const values = [nome, idade, nota_primeiro_semestre, nota_segundo_semestre, nome_professor, numero_sala, id];
+  pool.query(sql, values, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: 'Aluno não encontrado' });
+      return;
+    }
+    res.json(result.rows[0]);
+  });
+});
+
+// Rota para deletar um aluno por ID
+app.delete('/alunos/:id', (req, res) => {
+  const id = req.params.id;
+  const sql = 'DELETE FROM alunos WHERE id = $1 RETURNING *';
+  pool.query(sql, [id], (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    if (result.rows.length === 0) {
+      res.status(404).json({ error: 'Aluno não encontrado' });
+      return;
+    }
+    res.json({ message: 'Aluno deletado com sucesso' });
   });
 });
 
