2e580cfa910e6b9d2c7fa8c3b2536528c99e5cae
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-10-13 18:13:51 +0000

    atualização de depenências

--- DADOS_PROJETO/webserver.js
+++ DADOS_PROJETO/webserver.js
@@ -17,12 +17,20 @@ const server = app.listen(3000, () => {
   console.log('Servidor Node.js rodando na porta 3000');
 });
 
-// Trate o encerramento do servidor
+// Trate o encerramento do servidor e da conexão com o banco de dados
 process.on('SIGINT', () => {
   console.log('Encerrando o servidor...');
   server.close(() => {
     console.log('Servidor encerrado.');
+    pool.end((err) => {
+      if (err) {
+        console.error('Erro ao encerrar a conexão com o banco de dados:', err);
+      } else {
+        console.log('Conexão com o banco de dados encerrada.');
+      }
+    });
   });
 });
+
 
 
