26e5035ea7096471beeac2d1a46a67360be1fb44
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-10-13 18:19:07 +0000

    atualização de depenências

--- DADOS_PROJETO/routes/alunos.js
+++ DADOS_PROJETO/routes/alunos.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const alunosController = require('../controllers/alunosController');
+const alunosController = require('./controllers/alunosControllers');
 
 // Rota para criar um novo aluno
 router.post('/', alunosController.criarAluno);
