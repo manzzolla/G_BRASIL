99e67aa9cadf07908b4fea4b6641c4061f684e77
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-10-13 18:35:30 +0000

    Nova estrutura de código

--- DADOS_PROJETO/webserver.js
+++ DADOS_PROJETO/webserver.js
@@ -6,8 +6,16 @@ const alunosRoutes = require('./routes/alunos'); // Importe as rotas dos alunos
 
 app.use(bodyParser.json());
 
-// Configurar a conexão com o banco de dados PostgreSQL
-const pool = require('./config/databaseConfig');
+// Configuração da conexão com o banco de dados
+const pool = new Pool({
+  user: 'banco_de_dados_escola_user',
+  host: 'dpg-ckjjl99jrl0c73dfbakg-a.oregon-postgres.render.com',
+  database: 'banco_de_dados_escola',
+  password: 'OT3KvnLC880SWkCIWPwiftUYvJpUVS9h',
+  port: 5432,
+});
+
+module.exports = pool;
 
 // Use as rotas dos alunos
 app.use('/alunos', alunosRoutes);
