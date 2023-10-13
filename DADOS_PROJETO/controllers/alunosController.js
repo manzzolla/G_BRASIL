7325a146933bc3285b9a9ced75ff63e2e4a5fe84
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-10-13 18:39:19 +0000

    Nova estrutura de código

--- DADOS_PROJETO/controllers/alunosController.js
+++ DADOS_PROJETO/controllers/alunosController.js
@@ -1,14 +1,5 @@
 // Lógica de controle para recursos de alunos
 
-// Configuração da conexão com o banco de dados
-const pool = new Pool({
-  user: 'banco_de_dados_escola_user',
-  host: 'dpg-ckjjl99jrl0c73dfbakg-a.oregon-postgres.render.com',
-  database: 'banco_de_dados_escola',
-  password: 'OT3KvnLC880SWkCIWPwiftUYvJpUVS9h',
-  port: 5432,
-});
-
 const { Pool } = require('pg');
 
 // Função para criar um novo aluno
