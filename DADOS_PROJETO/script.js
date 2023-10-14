dad8095632cb96f677267ec7001e5d43d5003844
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-10-14 21:13:28 +0000

    Atualização de front end em script.js 1.1

--- DADOS_PROJETO/script.js
+++ DADOS_PROJETO/script.js
@@ -1,65 +1,29 @@
-const axios = require('axios');
-
-// Função para carregar a lista de alunos
+// Função para listar todos os alunos na tabela
 async function listarAlunos() {
   try {
-    const response = await axios.get('/alunos'); // Rota para listar alunos
+    const response = await axios.get('/alunos');
     const alunos = response.data;
 
-    // Limpa a tabela de alunos
-    const tabela = document.getElementById("alunos-table");
-    tabela.innerHTML = '';
+    const tabela = document.getElementById('alunos-table');
+    tabela.innerHTML = ''; // Limpa a tabela antes de adicionar os dados
 
-    // Preenche a tabela com os dados dos alunos
     alunos.forEach((aluno) => {
-      const row = tabela.insertRow();
-      row.innerHTML = `
-        <td>${aluno.id}</td>
-        <td>${aluno.nome}</td>
-        <td>${aluno.idade}</td>
-        <td>${aluno.nota_primeiro_semestre}</td>
-        <td>${aluno.nota_segundo_semestre}</td>
-        <td>${aluno.nome_professor}</td>
-        <td>${aluno.sala}</td>
-        <td>
-          <button onclick="editarAluno(${aluno.id})">Editar</button>
-          <button onclick="excluirAluno(${aluno.id})">Excluir</button>
-        </td>
-      `;
+      const newRow = tabela.insertRow(-1);
+      newRow.innerHTML = `<td>${aluno.id}</td><td>${aluno.nome}</td><td>${aluno.idade}</td><td>${aluno.nota_primeiro_semestre}</td><td>${aluno.nota_segundo_semestre}</td><td>${aluno.nome_professor}</td><td>${aluno.sala}</td><td><button onclick="editarAluno(${aluno.id})">Editar</button><button onclick="excluirAluno(${aluno.id})">Excluir</button></td>`;
     });
   } catch (error) {
     console.error('Erro ao listar alunos:', error);
   }
 }
 
-// Função para adicionar um novo aluno
-async function adicionarAluno() {
-  const nome = document.getElementById('nome').value;
-  const idade = parseInt(document.getElementById('idade').value);
-  const nota_primeiro_semestre = parseFloat(document.getElementById('nota1').value);
-  const nota_segundo_semestre = parseFloat(document.getElementById('nota2').value);
-  const nome_professor = document.getElementById('professor').value;
-  const sala = document.getElementById('sala').value;
-
+// Função para adicionar um aluno
+async function adicionarAluno(aluno) {
   try {
-    const response = await axios.post('/alunos', {
-      nome,
-      idade,
-      nota_primeiro_semestre,
-      nota_segundo_semestre,
-      nome_professor,
-      sala,
-    });
+    const response = await axios.post('/alunos', aluno);
 
-    // Limpa os campos de entrada
-    document.getElementById('nome').value = '';
-    document.getElementById('idade').value = '';
-    document.getElementById('nota1').value = '';
-    document.getElementById('nota2').value = '';
-    document.getElementById('professor').value = '';
-    document.getElementById('sala').value = '';
-
-    listarAlunos(); // Atualiza a tabela após adicionar um aluno
+    if (response.status === 201) {
+      listarAlunos(); // Atualiza a tabela após a adição
+    }
   } catch (error) {
     console.error('Erro ao adicionar aluno:', error);
   }
@@ -67,20 +31,44 @@ async function adicionarAluno() {
 
 // Função para editar um aluno
 async function editarAluno(id) {
-  // Implemente a função de edição conforme necessário
+  // Implemente a lógica para preencher o formulário de edição com os dados do aluno selecionado
+  // e permitir a edição. Após a edição, envie os dados atualizados para a API.
 }
 
 // Função para excluir um aluno
 async function excluirAluno(id) {
-  if (confirm('Tem certeza de que deseja excluir este aluno?')) {
-    try {
-      await axios.delete(`/alunos/${id}`);
-      listarAlunos(); // Atualiza a tabela após excluir um aluno
-    } catch (error) {
-      console.error('Erro ao excluir aluno:', error);
+  try {
+    const response = await axios.delete(`/alunos/${id}`);
+
+    if (response.status === 200) {
+      listarAlunos(); // Atualiza a tabela após a exclusão
     }
+  } catch (error) {
+    console.error('Erro ao excluir aluno:', error);
   }
 }
 
-// Carrega a lista de alunos ao iniciar a página
+// Função para adicionar um aluno quando o formulário é enviado
+document.getElementById('add-aluno-form').addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const nome = document.getElementById('nome').value;
+  const idade = parseInt(document.getElementById('idade').value);
+  const nota_primeiro_semestre = parseFloat(document.getElementById('nota1').value);
+  const nota_segundo_semestre = parseFloat(document.getElementById('nota2').value);
+  const nome_professor = document.getElementById('professor').value;
+  const sala = document.getElementById('sala').value;
+
+  const novoAluno = {
+    nome,
+    idade,
+    nota_primeiro_semestre,
+    nota_segundo_semestre,
+    nome_professor,
+    sala,
+  };
+
+  adicionarAluno(novoAluno);
+});
+
+// Carrega a lista de alunos quando a página é carregada
 listarAlunos();
