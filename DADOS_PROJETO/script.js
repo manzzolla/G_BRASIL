313efcb6d8f5bf3337fd12942f8e5d0c91ca8fa6
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-10-14 21:48:11 +0000

    Atualização do front end 1.6

--- DADOS_PROJETO/script.js
+++ DADOS_PROJETO/script.js
@@ -2,12 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const fetch = require('node-fetch');
+require = require('esm')(module);
 
 
 app.use(cors());
-
-
-
 
 // Função para criar um novo aluno
 function criarAluno() {
