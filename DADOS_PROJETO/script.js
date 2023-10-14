1ff61e69b3775215f6bfbd69dd9d7fdbad95be05
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-10-14 21:32:24 +0000

    Atualização do front end 1.3

--- DADOS_PROJETO/script.js
+++ DADOS_PROJETO/script.js
@@ -1,74 +1,98 @@
-// Função para listar todos os alunos na tabela
-async function listarAlunos() {
-  try {
-    const response = await axios.get('/alunos');
-    const alunos = response.data;
-
-    const tabela = document.getElementById('alunos-table');
-    tabela.innerHTML = ''; // Limpa a tabela antes de adicionar os dados
-
-    alunos.forEach((aluno) => {
-      const newRow = tabela.insertRow(-1);
-      newRow.innerHTML = `<td>${aluno.id}</td><td>${aluno.nome}</td><td>${aluno.idade}</td><td>${aluno.nota_primeiro_semestre}</td><td>${aluno.nota_segundo_semestre}</td><td>${aluno.nome_professor}</td><td>${aluno.sala}</td><td><button onclick="editarAluno(${aluno.id})">Editar</button><button onclick="excluirAluno(${aluno.id})">Excluir</button></td>`;
-    });
-  } catch (error) {
-    console.error('Erro ao listar alunos:', error);
-  }
-}
-
-// Função para adicionar um aluno
-async function adicionarAluno(aluno) {
-  try {
-    const response = await axios.post('/alunos', aluno);
-
-    if (response.status === 201) {
-      listarAlunos(); // Atualiza a tabela após a adição
-    }
-  } catch (error) {
-    console.error('Erro ao adicionar aluno:', error);
-  }
-}
-
-// Função para editar um aluno
-async function editarAluno(id) {
-  // Implemente a lógica para preencher o formulário de edição com os dados do aluno selecionado
-  // e permitir a edição. Após a edição, envie os dados atualizados para a API.
-}
-
-// Função para excluir um aluno
-async function excluirAluno(id) {
-  try {
-    const response = await axios.delete(`/alunos/${id}`);
-
-    if (response.status === 200) {
-      listarAlunos(); // Atualiza a tabela após a exclusão
-    }
-  } catch (error) {
-    console.error('Erro ao excluir aluno:', error);
-  }
-}
-
-// Função para adicionar um aluno quando o formulário é enviado
-document.getElementById('add-aluno-form').addEventListener('submit', async (event) => {
-  event.preventDefault();
+// Função para criar um novo aluno
+function criarAluno() {
   const nome = document.getElementById('nome').value;
   const idade = parseInt(document.getElementById('idade').value);
-  const nota_primeiro_semestre = parseFloat(document.getElementById('nota1').value);
-  const nota_segundo_semestre = parseFloat(document.getElementById('nota2').value);
-  const nome_professor = document.getElementById('professor').value;
+  const nota1 = parseFloat(document.getElementById('nota1').value);
+  const nota2 = parseFloat(document.getElementById('nota2').value);
+  const professor = document.getElementById('professor').value;
   const sala = document.getElementById('sala').value;
 
-  const novoAluno = {
+  // Crie um objeto com os dados do aluno
+  const aluno = {
     nome,
     idade,
-    nota_primeiro_semestre,
-    nota_segundo_semestre,
-    nome_professor,
+    nota_primeiro_semestre: nota1,
+    nota_segundo_semestre: nota2,
+    nome_professor: professor,
     sala,
   };
 
-  adicionarAluno(novoAluno);
-});
+  // Faça uma solicitação POST para a API para adicionar o aluno
+  fetch('sua_url_da_api/alunos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(aluno),
+  })
+    .then((response) => response.json())
+    .then((novoAluno) => {
+      // Aqui você pode atualizar a tabela ou fazer o que desejar com o novo aluno
+      console.log('Novo aluno adicionado:', novoAluno);
+      // Limpe os campos do formulário após adicionar o aluno
+      document.getElementById('nome').value = '';
+      document.getElementById('idade').value = '';
+      document.getElementById('nota1').value = '';
+      document.getElementById('nota2').value = '';
+      document.getElementById('professor').value = '';
+      document.getElementById('sala').value = '';
+      // Chame a função para listar os alunos novamente
+      listarAlunos();
+    })
+    .catch((error) => {
+      console.error('Erro ao adicionar aluno:', error);
+    });
+}
 
-// Carrega a lista de alunos quando a página é carregada
+// Função para listar alunos
+function listarAlunos() {
+  // Faça uma solicitação GET para a API para obter a lista de alunos
+  fetch('sua_url_da_api/alunos')
+    .then((response) => response.json())
+    .then((alunos) => {
+      // Aqui você pode preencher a tabela com os alunos
+      const tabela = document.getElementById('alunos-table');
+      const tbody = tabela.querySelector('tbody');
+      tbody.innerHTML = ''; // Limpa os dados anteriores
+
+      alunos.forEach((aluno) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${aluno.id}</td>
+          <td>${aluno.nome}</td>
+          <td>${aluno.idade}</td>
+          <td>${aluno.nota_primeiro_semestre}</td>
+          <td>${aluno.nota_segundo_semestre}</td>
+          <td>${aluno.nome_professor}</td>
+          <td>${aluno.sala}</td>
+          <td>
+            <button onclick="editarAluno(${aluno.id})">Editar</button>
+            <button onclick="excluirAluno(${aluno.id})">Excluir</button>
+          </td>
+        `;
+        tbody.appendChild(row);
+      });
+    })
+    .catch((error) => {
+      console.error('Erro ao listar alunos:', error);
+    });
+}
+
+// Função para excluir um aluno
+function excluirAluno(id) {
+  // Faça uma solicitação DELETE para a API para excluir o aluno com o ID especificado
+  fetch(`sua_url_da_api/alunos/${id}`, {
+    method: 'DELETE',
+  })
+    .then(() => {
+      console.log(`Aluno com ID ${id} excluído.`);
+      // Atualize a lista de alunos após a exclusão
+      listarAlunos();
+    })
+    .catch((error) => {
+      console.error(`Erro ao excluir aluno com ID ${id}:`, error);
+    });
+}
+
+// Chame a função para listar alunos ao carregar a página
 listarAlunos();
