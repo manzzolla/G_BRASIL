31f799129e1f1cdb3bf48084fac73084f07201c7
Edison Manzzolla Junior <91670433+manzzolla@users.noreply.github.com>
2023-10-14 16:19:11 +0000

    Correção de index.html e script.js

--- DADOS_PROJETO/script.js
+++ DADOS_PROJETO/script.js
@@ -1,70 +1,78 @@
-const alunosTable = document.getElementById('alunos-table');
-const addAlunoForm = document.getElementById('add-aluno-form');
+// Exemplo de dados iniciais (pode ser carregado a partir da API)
+let alunos = [];
 
-// Função para listar alunos
-async function listarAlunos() {
-  const response = await fetch('https://escola-db.onrender.com/alunos');
-  const alunos = await response.json();
+// Função para adicionar um aluno
+function adicionarAluno() {
+  const nome = document.getElementById("nome").value;
+  const idade = document.getElementById("idade").value;
+  const nota1 = document.getElementById("nota1").value;
+  const nota2 = document.getElementById("nota2").value;
+  const professor = document.getElementById("professor").value;
+  const sala = document.getElementById("sala").value;
 
-  // Preencher a tabela
-  const tbody = alunosTable.querySelector('tbody');
-  tbody.innerHTML = '';
+  // Valide os dados aqui, certificando-se de que não sejam nulos, vazios, etc.
 
-  alunos.forEach((aluno) => {
-    const row = document.createElement('tr');
+  const novoAluno = {
+    nome,
+    idade,
+    nota1,
+    nota2,
+    professor,
+    sala,
+  };
+
+  // Adicione o novo aluno à lista
+  alunos.push(novoAluno);
+
+  // Limpe os campos de entrada
+  document.getElementById("nome").value = "";
+  document.getElementById("idade").value = "";
+  document.getElementById("nota1").value = "";
+  document.getElementById("nota2").value = "";
+  document.getElementById("professor").value = "";
+  document.getElementById("sala").value = "";
+
+  // Atualize a tabela
+  listarAlunos();
+}
+
+// Função para listar os alunos na tabela
+function listarAlunos() {
+  const tabela = document.getElementById("alunos-table");
+  const tbody = tabela.querySelector("tbody");
+
+  // Limpe a tabela
+  tbody.innerHTML = "";
+
+  // Preencha a tabela com os dados dos alunos
+  alunos.forEach((aluno, index) => {
+    const row = document.createElement("tr");
     row.innerHTML = `
-      <td>${aluno.id}</td>
+      <td>${index}</td>
       <td>${aluno.nome}</td>
       <td>${aluno.idade}</td>
-      <td>${aluno.nota_primeiro_semestre}</td>
-      <td>${aluno.nota_segundo_semestre}</td>
-      <td>${aluno.nome_professor}</td>
+      <td>${aluno.nota1}</td>
+      <td>${aluno.nota2}</td>
+      <td>${aluno.professor}</td>
       <td>${aluno.sala}</td>
-      <td>
-        <button class="editar-aluno" data-id="${aluno.id}">Editar</button>
-        <button class="excluir-aluno" data-id="${aluno.id}">Excluir</button>
-      </td>
+      <td><button onclick="excluirAluno(${index})">Excluir</button></td>
     `;
-
     tbody.appendChild(row);
   });
 }
 
-// Evento de envio do formulário para adicionar aluno
-addAlunoForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
-
-  const nome = document.getElementById('nome').value;
-  const idade = document.getElementById('idade').value;
-  const nota1 = document.getElementById('nota1').value;
-  const nota2 = document.getElementById('nota2').value;
-  const professor = document.getElementById('professor').value;
-  const sala = document.getElementById('sala').value;
-
-  // Faça uma solicitação POST para adicionar o aluno
-  const response = await fetch('https://escola-db.onrender.com/alunos', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      nome,
-      idade,
-      nota_primeiro_semestre: nota1,
-      nota_segundo_semestre: nota2,
-      nome_professor: professor,
-      sala,
-    }),
-  });
-
-  // Limpe o formulário após a adição do aluno
-  addAlunoForm.reset();
-
-  // Atualize a tabela
+// Função para excluir um aluno
+function excluirAluno(index) {
+  alunos.splice(index, 1);
   listarAlunos();
-});
+}
 
-// Carregue a lista de alunos no início
+// Liste os alunos iniciais (se necessário)
 listarAlunos();
 
-
+// Adicione um ouvinte de evento para o formulário de adicionar aluno
+const form = document.getElementById("add-aluno-form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // Impede o envio do formulário
+  adicionarAluno();
+});
